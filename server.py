51f8d6e05df2bacc2c4938ea45de1a5e8005ee75
Asahi Kuroki <kurok011@umn.edu>
2023-02-15 12:29:13 -0600

    set up Oauth

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 from flask import *
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 
-
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 @app.route("/")
 def home():
